f6bf1f3824f81eded98748fd70c298fd54e66bf8
okila-ok <okilasalieva@gmail.com>
2023-03-26 10:51:02 -0700

    - added solution and tests for homework 10.1 task one

---
+++ src/main/java/org/example/homeworkten1/YesOrNo.java
@@ -0,0 +1,19 @@
+package org.example.homeworkten1;
+
+public class YesOrNo {
+    public static String boolToWord(boolean b)
+    {
+//        if (b == true){
+//            return "Yes";
+//        }
+//        else {
+//            return "No";
+//        }
+
+//        if (b)
+//          return "Yes";
+//        return "No";
+
+        return b? "Yes": "No";
+    }
+}
